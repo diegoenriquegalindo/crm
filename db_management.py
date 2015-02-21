ce67121fd024045179ea03eaf2a049b7f647f5e6
pagalindo <pagalindo@gmail.com>
2015-02-21 14:13:08 -0500

    update to db_management for Vendor.

--- db_management.py
+++ db_management.py
@@ -1,6 +1,5 @@
 import pickle
-from django.contrib.auth.models import User
-from home.models import Employee, Customer
+from home.models import Vendor, Customer
 from sets import Set
 
 def get_username(first_name):
@@ -13,23 +12,21 @@ def get_username(first_name):
     username_set.add(username)
     return username
 
-def add_employee(employee):
-    first_name = employee[1].split()[0]
+def add_vendor(vendor):
+    first_name = vendor[1].split()[0]
     username = get_username(first_name)
-    new_user = User.objects.create_user(username,'','piggy')
-    new_employee = Employee()
-    new_employee.user = new_user
-    new_employee.employee_id = employee[0]
-    new_employee.name = employee[1]
-    citizenship_card = employee[2]
+    new_vendor = Vendor.objects.create_user(username,'','piggy')
+    new_vendor.vendor_id = vendor[0]
+    new_vendor.name = vendor[1]
+    citizenship_card = vendor[2]
     try: citizenship_card = int(citizenship_card)
     except ValueError: citizenship_card = None
-    new_employee.citizenship_card = citizenship_card
-    commission = employee[3]
+    new_vendor.citizenship_card = citizenship_card
+    commission = vendor[3]
     try: commission = float(commission)
     except ValueError: commission = None
-    new_employee.commission = commission
-    new_employee.save()
+    new_vendor.commission = commission
+    new_vendor.save()
 
 def add_customer(customer):
     new_customer = Customer()
@@ -38,20 +35,31 @@ def add_customer(customer):
     new_customer.contact = customer[4]
     new_customer.position = customer[5]
     new_customer.save()
+    # new_customer.owner = User.objects.filter(username='pagalin')
+    # new_customer.save()
+
+def join_customer_vendor():
+    pass
 
 f = open("vendedores.pkl")
-employees = pickle.load(f)
+vendors = pickle.load(f)
 username_set = Set()
-for employee in employees[1]:
-    if employee[0] == 'ND': continue
-    add_employee(employee)
+for vendor in vendors[1]:
+    if vendor[0] == 'ND': continue
+    add_vendor(vendor)
 
 f.close()
 
 f = open("listaClientes00.pkl")
 customers = pickle.load(f)
-print len(customers[1])
 for customer in customers[1]:
     add_customer(customer)
 
 f.close()
+
+# f = open("listaClientes00.pkl")
+# customers = pickle.load(f)
+# add_customer(customers[1][0])
+
+# f.close()
+
